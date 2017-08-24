7ac5e2994be6a98f0482f50353d9aa8b4e6cd497
WindAFlame <dev.windaflame@gmail.com>
2017-08-24 15:48:02 +0200

    Add .scss into task watch.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,53 @@
 // Main Dependancies
-var gulp = require('gulp');
+let gulp = require('gulp');
+// Dependancies - Concat
+let concat = require('gulp-concat');
+// Dependancies - SASS
+let sass = require('gulp-sass');
 // Dependancies - Livereload
-var connect = require('gulp-connect');
+let connect = require('gulp-connect');
 
+/**
+ * Task to host project on localhost:8080 with livereload.
+ * Target repository : './src'
+ */
 gulp.task('connect', function() {
-  connect.server({
-    root: 'src',
-    livereload: true
-  });
+    connect.server({
+        port: 8888,
+        root: 'src',
+        livereload: true
+    });
 });
- 
+
+/**
+ * Watch html file in two sources.
+ */
 gulp.task('html', function () {
-  gulp.src('./src/index.html')
-    .pipe(connect.reload());
-  gulp.src('./src/views/**/*.html')
-    .pipe(connect.reload());
+    gulp.src('./src/index.html')
+        .pipe(connect.reload());
+    gulp.src('./src/app/**/*.html')
+        .pipe(connect.reload());
 });
- 
+
+/**
+ * Watch scss file in one source.
+ * Log error when it appeared.
+ * ?
+ */
+gulp.task('sass', function () {
+    return gulp.src('./src/app/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('./src/'))
+        .pipe(connect.reload());
+});
+
+/**
+ * Watch and ?
+ */
 gulp.task('watch', function () {
-  gulp.watch(['./src/index.html','./src/views/**/*.html'], ['html']);
+    gulp.watch(['./src/index.html','./src/app/**/*.html'], ['html']);
+    gulp.watch('./src/app/**/*.scss', ['sass']);
 });
 
 gulp.task('serve', ['connect', 'watch']);
